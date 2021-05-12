eae240319b5f26b7dc5a98a7dc6cd22a302ba04d
George-Alexandru Gologan <aggologan1999@gmail.com>
2021-05-12 23:20:19 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,36 @@ class _CurrencyConverterAppState extends State<CurrencyConverterApp>
   final Color _secondaryColor = Color(0xffDBDAD4);
   final String _appTitle = "Currency converter";
   final String _convertButtonText = "Convert";
-  final String _textFieldLabelText = "Amount";
+  final String _textFieldHint = "Amount (GBP)";
+  final String inputNotOkayToastMessage = "Please enter a valid amount of money";
   final String _textFieldRegex = "[0-9.]";
   final String _moneyImageSource = "assets/images/money.svg";
   String _convertedValue = "";
 
+  void clearController()
+  {
+    if(_textFieldController.text.length > 0)
+      _textFieldController.clear();
+  }
+
+  double convertToRON(double amountOfMoney)
+  {
+    return amountOfMoney * 5.74;
+  }
+
   String handleDotFirstCharacter(String input) // if the first character of the input is a dot, than we add a 0 before it
   {
     return "0" + input;
+  }
+
+  String handleLimitFirstTwoDecimals(String input)
+  {
+    return input.substring(0, getPositionOfDot(input) + 2);
+  }
+
+  String handleRemoveZeroAsTheFirstCharacter(String input)
+  {
+    return input.substring(1);
   }
 
   String handleLimitOnlyOneDot(String input)
@@ -47,16 +69,6 @@ class _CurrencyConverterAppState extends State<CurrencyConverterApp>
     });
 
     return input.substring(0, positionOfLastDot);
-  }
-
-  String handleLimitFirstTwoDecimals(String input)
-  {
-    return input.substring(0, getPositionOfDot(input) + 2);
-  }
-
-  String handleRemoveZeroAsTheFirstCharacter(String input)
-  {
-    return input.substring(1);
   }
 
   int getNumberOfDots(String input)
@@ -102,33 +114,6 @@ class _CurrencyConverterAppState extends State<CurrencyConverterApp>
     return numberOfDecimals;
   }
 
-  int checkIfInputContainsMoreThanTwoDecimals(String input)
-  {
-    int currentPosition = -1;
-    int dotPosition = -1;
-
-    if(input.length > 4)
-    {
-      input.runes.forEach((element)
-      {
-        var character = String.fromCharCode(element);
-
-        ++currentPosition;
-
-        if(character == '.')
-          dotPosition = currentPosition;
-      });
-
-      if(getNumberOfDots(input) > 1)
-        currentPosition = -2;
-      else if(currentPosition - dotPosition < 3)
-        currentPosition = -1;
-      else currentPosition = dotPosition + 2;
-    }
-
-    return currentPosition;
-  }
-
   bool inputIsOK(String inputValue)
   {
     int currentPosition = -1;
@@ -160,14 +145,59 @@ class _CurrencyConverterAppState extends State<CurrencyConverterApp>
     return true;
   }
 
+  void checkIfThereIsSomethingWrongWithTheInput(String input)
+  {
+    if(input.length == 1 && input[0] == '.')
+    {
+      _textFieldController.text = handleDotFirstCharacter(_textFieldController.text);
+      _textFieldController.selection = TextSelection.fromPosition(TextPosition(offset: _textFieldController.text.length));
+    }
+    else if(getNumberOfDots(input) > 1)
+    {
+      _textFieldController.text = handleLimitOnlyOneDot(_textFieldController.text);
+      _textFieldController.selection = TextSelection.fromPosition(TextPosition(offset: _textFieldController.text.length));
+    }
+    else if(getNumberOfDecimals(input) > 2)
+    {
+      _textFieldController.text = handleLimitFirstTwoDecimals(_textFieldController.text);
+      _textFieldController.selection = TextSelection.fromPosition(TextPosition(offset: _textFieldController.text.length));
+    }
+    else if(input.length > 1 && input[0] == '0' && input[1] != '.')
+    {
+      _textFieldController.text = handleRemoveZeroAsTheFirstCharacter(_textFieldController.text);
+      _textFieldController.selection = TextSelection.fromPosition(TextPosition(offset: _textFieldController.text.length));
+    }
+  }
+
   void setConvertedValue(String inputValue) // the input is ok if it isn't empty and its last character isn't a dot
   {
     setState(()
     {
       if(inputIsOK(inputValue))
+      {
+        inputValue = convertToRON(double.tryParse(inputValue)).toString();
+
+        if(getNumberOfDecimals(inputValue) > 2)
+          inputValue = handleLimitFirstTwoDecimals(inputValue);
+
         _convertedValue = inputValue;
-      else _convertedValue = "";
+      }
+      else
+      {
+        _convertedValue = "";
+        showMessage('$inputNotOkayToastMessage');
+      }
     });
+  }
+
+  void showMessage(String message)
+  {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      backgroundColor: _primaryColor,
+      textColor: _secondaryColor
+    );
   }
 
   @override
@@ -185,102 +215,102 @@ class _CurrencyConverterAppState extends State<CurrencyConverterApp>
           child: Column(
             children: [
               Expanded(
-                  flex: 4,
-                  child: Container(
-                    child: FittedBox(
-                      fit: BoxFit.fill,
-                      child: SvgPicture.asset('$_moneyImageSource'),
+                flex: 4,
+                child: Container(
+                  child: FittedBox(
+                    fit: BoxFit.fill,
+                    child: SvgPicture.asset('$_moneyImageSource'),
                     ),
                   )
               ),
               Expanded(
-                  flex: 6,
-                  child: Container(
-                    color: _secondaryColor,
-                    child: Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          FractionallySizedBox(
-                            widthFactor: .6,
-                            child: Column(
-                              children: [
-                                TextField(
+                flex: 6,
+                child: Container(
+                  color: _secondaryColor,
+                  child: Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        FractionallySizedBox(
+                          widthFactor: .75,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                                child: TextField(
                                   controller: _textFieldController,
-                                  onChanged: (text)
-                                  {
-                                    if(text.length == 1 && text[0] == '.')
-                                    {
-                                      _textFieldController.text = handleDotFirstCharacter(_textFieldController.text);
-                                      _textFieldController.selection = TextSelection.fromPosition(TextPosition(offset: _textFieldController.text.length));
-                                    }
-                                    else if(getNumberOfDots(text) > 1)
-                                    {
-                                      _textFieldController.text = handleLimitOnlyOneDot(_textFieldController.text);
-                                      _textFieldController.selection = TextSelection.fromPosition(TextPosition(offset: _textFieldController.text.length));
-                                    }
-                                    else if(getNumberOfDecimals(text) > 2)
-                                    {
-                                      _textFieldController.text = handleLimitFirstTwoDecimals(_textFieldController.text);
-                                      _textFieldController.selection = TextSelection.fromPosition(TextPosition(offset: _textFieldController.text.length));
-                                    }
-                                    else if(text.length > 1 && text[0] == '0' && text[1] != '.')
-                                    {
-                                      _textFieldController.text = handleRemoveZeroAsTheFirstCharacter(_textFieldController.text);
-                                      _textFieldController.selection = TextSelection.fromPosition(TextPosition(offset: _textFieldController.text.length));
-                                    }
-                                  },
+                                  onChanged: (text) => checkIfThereIsSomethingWrongWithTheInput(text),
                                   cursorColor: _primaryColor,
                                   decoration: InputDecoration(
-                                      labelText: '$_textFieldLabelText',
-                                      labelStyle: TextStyle(
-                                          color: _primaryColor,
-                                          fontSize: 20
-                                      ),
+                                    contentPadding: EdgeInsets.zero,
+                                    hintText: '$_textFieldHint',
+                                    hintStyle: TextStyle(
+                                      color: _primaryColor,
+                                      fontSize: 20
+                                    ),
+                                    suffix: IconButton(
+                                      icon: Icon(Icons.clear),
+                                      onPressed: clearController,
+                                    ),
                                     focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: _primaryColor)),
                                     enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: _primaryColor)),
                                   ),
                                   keyboardType: TextInputType.numberWithOptions(decimal: true),
                                   style: TextStyle(
-                                      color: _primaryColor,
-                                      fontSize: 20
+                                    color: _primaryColor,
+                                    fontSize: 20
                                   ),
                                   inputFormatters: [FilteringTextInputFormatter.allow(RegExp('$_textFieldRegex'))],
                                 ),
-                                Wrap(
-                                  children: [
-                                    OutlinedButton(
+                              ),
+                              Wrap(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                                    child: OutlinedButton(
                                       style: OutlinedButton.styleFrom(
-                                          primary: _primaryColor,
-                                          side: BorderSide(
-                                              width: 2,
-                                              color: _primaryColor
-                                          )
+                                        primary: _primaryColor,
+                                        side: BorderSide(
+                                          width: 2,
+                                          color: _primaryColor
+                                        )
                                       ),
-                                      onPressed: ()
-                                      {
-                                        final textFieldControllerText = _textFieldController.text;
-
-                                        setConvertedValue(textFieldControllerText);
-                                      },
-                                      child: Text('$_convertButtonText'),
-                                    )
-                                  ],
+                                      onPressed: () => setConvertedValue(_textFieldController.text),
+                                      child: Text(
+                                        '$_convertButtonText',
+                                        style: TextStyle(
+                                          color: _primaryColor,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                                child: Text(
+                                  "$_convertedValue",
+                                  style: TextStyle(
+                                    color: _primaryColor,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                                Text("$_convertedValue")
-                              ],
-                            ),
+                              )
+                            ],
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  )
+                  ),
+                )
               )
             ],
           ),
         ),
-        resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomInset: false,
       ),
     );
   }
