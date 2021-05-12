50ff2001691705d28152312d091670aabdf2a3a9
George-Alexandru Gologan <aggologan1999@gmail.com>
2021-05-12 20:38:59 +0300

    33% of the app is ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,287 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main()
 {
-  runApp(MyApp());
+  runApp(CurrencyConverterApp());
 }
 
-class MyApp extends StatelessWidget
+class CurrencyConverterApp extends StatefulWidget
 {
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context)
+  _CurrencyConverterAppState createState() => _CurrencyConverterAppState();
+}
+
+class _CurrencyConverterAppState extends State<CurrencyConverterApp>
+{
+  final _textFieldController = TextEditingController();
+  final Color _primaryColor = Color(0xff3B4C58);
+  final Color _secondaryColor = Color(0xffDBDAD4);
+  final String _appTitle = "Currency converter";
+  final String _convertButtonText = "Convert";
+  final String _textFieldLabelText = "Amount";
+  final String _textFieldRegex = "[0-9.]";
+  final String _moneyImageSource = "assets/images/money.svg";
+  String _convertedValue = "";
+
+  String handleDotFirstCharacter(String input) // if the first character of the input is a dot, than we add a 0 before it
   {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return "0" + input;
   }
-}
 
-class MyHomePage extends StatefulWidget
-{
-  MyHomePage({Key key, this.title}) : super(key: key);
+  String handleLimitOnlyOneDot(String input)
+  {
+    int currentPosition = -1;
+    int positionOfLastDot = -1;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    input.runes.forEach((element)
+    {
+      var character = String.fromCharCode(element);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+      ++currentPosition;
 
-  final String title;
+      if(character == '.')
+        positionOfLastDot = currentPosition;
+    });
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+    return input.substring(0, positionOfLastDot);
+  }
 
-class _MyHomePageState extends State<MyHomePage>
-{
-  int _counter = 0;
+  String handleLimitFirstTwoDecimals(String input)
+  {
+    return input.substring(0, getPositionOfDot(input) + 2);
+  }
 
-  void _incrementCounter()
+  String handleRemoveZeroAsTheFirstCharacter(String input)
+  {
+    return input.substring(1);
+  }
+
+  int getNumberOfDots(String input)
+  {
+    int numberOfDots = 0;
+
+    if(input.length > 0)
+      input.runes.forEach((element)
+      {
+        if(String.fromCharCode(element) == '.')
+          ++numberOfDots;
+      });
+
+    return numberOfDots;
+  }
+
+  int getPositionOfDot(String input)
+  {
+    int positionOfDot = -1;
+    int currentPosition = 0;
+
+    input.runes.forEach((element)
+    {
+      ++currentPosition;
+
+      if(String.fromCharCode(element) == '.')
+        positionOfDot = currentPosition;
+
+      });
+
+    return positionOfDot;
+  }
+
+  int getNumberOfDecimals(String input)
+  {
+    int numberOfDecimals = 0;
+    int inputLength = input.length;
+    int positionOfDot = getPositionOfDot(input);
+
+    if(getNumberOfDots(input) > 0)
+      numberOfDecimals = inputLength - positionOfDot;
+
+    return numberOfDecimals;
+  }
+
+  int checkIfInputContainsMoreThanTwoDecimals(String input)
+  {
+    int currentPosition = -1;
+    int dotPosition = -1;
+
+    if(input.length > 4)
+    {
+      input.runes.forEach((element)
+      {
+        var character = String.fromCharCode(element);
+
+        ++currentPosition;
+
+        if(character == '.')
+          dotPosition = currentPosition;
+      });
+
+      if(getNumberOfDots(input) > 1)
+        currentPosition = -2;
+      else if(currentPosition - dotPosition < 3)
+        currentPosition = -1;
+      else currentPosition = dotPosition + 2;
+    }
+
+    return currentPosition;
+  }
+
+  bool inputIsOK(String inputValue)
+  {
+    int currentPosition = -1;
+
+    if(inputValue.length == 0) // it's not ok if the input has no characters
+      return false;
+
+    if(inputValue.length == 1 && inputValue[0] != '.') // it's not ok if the input has 1 character and it's a dot
+      return true;
+
+    if(inputValue[0] == '0' && inputValue[1] != '.') // it's not ok if the first character is 0 and the second one isn't a dot (e.g: 01234)
+      return false;
+
+    if(inputValue[inputValue.length - 1] == '.') // it's not ok if the last character of the input is a dot
+      return false;
+
+    inputValue.runes.forEach((element) // looping through input's characters
+    {
+      var character = new String.fromCharCode(element);
+      ++currentPosition;
+
+      if(currentPosition == 0 && character == '.') // it's not ok if the first character is a dot (e.g: .1234)
+        return false;
+    });
+
+    if(getNumberOfDots(inputValue) > 1) // it's not ok if the input has more than one dot
+      return false;
+
+    return true;
+  }
+
+  void setConvertedValue(String inputValue) // the input is ok if it isn't empty and its last character isn't a dot
   {
     setState(()
     {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if(inputIsOK(inputValue))
+        _convertedValue = inputValue;
+      else _convertedValue = "";
     });
   }
 
   @override
   Widget build(BuildContext context)
   {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('$_appTitle'),
+          backgroundColor: _primaryColor,
+          centerTitle: true,
         ),
+        body: Container(
+          color: _secondaryColor,
+          child: Column(
+            children: [
+              Expanded(
+                  flex: 4,
+                  child: Container(
+                    child: FittedBox(
+                      fit: BoxFit.fill,
+                      child: SvgPicture.asset('$_moneyImageSource'),
+                    ),
+                  )
+              ),
+              Expanded(
+                  flex: 6,
+                  child: Container(
+                    color: _secondaryColor,
+                    child: Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          FractionallySizedBox(
+                            widthFactor: .6,
+                            child: Column(
+                              children: [
+                                TextField(
+                                  controller: _textFieldController,
+                                  onChanged: (text)
+                                  {
+                                    if(text.length == 1 && text[0] == '.')
+                                    {
+                                      _textFieldController.text = handleDotFirstCharacter(_textFieldController.text);
+                                      _textFieldController.selection = TextSelection.fromPosition(TextPosition(offset: _textFieldController.text.length));
+                                    }
+                                    else if(getNumberOfDots(text) > 1)
+                                    {
+                                      _textFieldController.text = handleLimitOnlyOneDot(_textFieldController.text);
+                                      _textFieldController.selection = TextSelection.fromPosition(TextPosition(offset: _textFieldController.text.length));
+                                    }
+                                    else if(getNumberOfDecimals(text) > 2)
+                                    {
+                                      _textFieldController.text = handleLimitFirstTwoDecimals(_textFieldController.text);
+                                      _textFieldController.selection = TextSelection.fromPosition(TextPosition(offset: _textFieldController.text.length));
+                                    }
+                                    else if(text.length > 1 && text[0] == '0' && text[1] != '.')
+                                    {
+                                      _textFieldController.text = handleRemoveZeroAsTheFirstCharacter(_textFieldController.text);
+                                      _textFieldController.selection = TextSelection.fromPosition(TextPosition(offset: _textFieldController.text.length));
+                                    }
+                                  },
+                                  cursorColor: _primaryColor,
+                                  decoration: InputDecoration(
+                                      labelText: '$_textFieldLabelText',
+                                      labelStyle: TextStyle(
+                                          color: _primaryColor,
+                                          fontSize: 20
+                                      ),
+                                    focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: _primaryColor)),
+                                    enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: _primaryColor)),
+                                  ),
+                                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                                  style: TextStyle(
+                                      color: _primaryColor,
+                                      fontSize: 20
+                                  ),
+                                  inputFormatters: [FilteringTextInputFormatter.allow(RegExp('$_textFieldRegex'))],
+                                ),
+                                Wrap(
+                                  children: [
+                                    OutlinedButton(
+                                      style: OutlinedButton.styleFrom(
+                                          primary: _primaryColor,
+                                          side: BorderSide(
+                                              width: 2,
+                                              color: _primaryColor
+                                          )
+                                      ),
+                                      onPressed: ()
+                                      {
+                                        final textFieldControllerText = _textFieldController.text;
+
+                                        setConvertedValue(textFieldControllerText);
+                                      },
+                                      child: Text('$_convertButtonText'),
+                                    )
+                                  ],
+                                ),
+                                Text("$_convertedValue")
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+              )
+            ],
+          ),
+        ),
+        resizeToAvoidBottomInset: false,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:currency_converter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(CurrencyConverterApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
